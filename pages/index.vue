a5d4f3baaf9f338aea27b682831c1ff383844c55
Tam Mai <tam.mai@bigin.vn>
2022-11-10 15:28:25 +0700

    feat(layout): edit sample layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,34 @@
 <template>
   <b-container class="min-h-screen">
     <b-header>
-      <b-row :gutter="2" items="center" class="flex-1">
-        <b-col span="auto">
-          <b-image src="/favicon.png" class="h-8 w-8 rounded-sm" />
-        </b-col>
-        <b-col span="auto" class="text-3xl font-semibold"
-          >Nuxt Boilerplate</b-col
+      <div class="flex items-center">
+        <b-image src="/favicon.png" class="h-8 w-8 rounded-sm" />
+        <h1 class="text-3xl ml-2">Nuxt Boilerplate</h1>
+      </div>
+      <div class="ml-auto">
+        <b-tooltip
+          :open-delay="150"
+          :content="darkMode ? 'Switch to light mode' : 'Switch to dark mode'"
+          placement="right"
         >
-        <b-col></b-col>
-        <b-col span="auto">
-          <b-tooltip
-            :content="darkMode ? 'Switch to light mode' : 'Switch to dark mode'"
-            placement="right"
-          >
-            <span
-              :class="[
-                darkMode
-                  ? 'i-fluent:weather-moon-16-regular'
-                  : 'i-fluent:weather-sunny-16-regular',
-                'text-2xl',
-                'block',
-                'w-5',
-                'h-5',
-                'b-c-neutral-7',
-                'hover:cursor-pointer',
-                'hover:b-c-primary',
-              ]"
-              @click="toggleDarkMode"
-            />
-          </b-tooltip>
-        </b-col>
-      </b-row>
+          <span
+            :class="[
+              darkMode
+                ? 'i-fluent:weather-moon-16-regular'
+                : 'i-fluent:weather-sunny-16-regular',
+              'text-2xl',
+              'block',
+              'w-5',
+              'h-5',
+              'b-c-neutral-7',
+              'hover:cursor-pointer',
+              'hover:b-c-primary',
+              'transition-colors',
+            ]"
+            @click="toggleDarkMode"
+          />
+        </b-tooltip>
+      </div>
     </b-header>
     <b-main class="flex flex-col items-center justify-center">
       <h1>BigIn</h1>
@@ -54,34 +51,5 @@ import { ArrowRight } from "@bigin/icons-vue";
 import { useDark, useToggle } from "@vueuse/core";
 
 const darkMode = useDark();
-
 const toggleDarkMode = () => useToggle(darkMode)();
-
-const chartData = ref({
-  labels: ["Spring", "Summer", "Fall", "Winter"],
-  datasets: [
-    {
-      label: "Dog",
-      data: [40, 20, 12, 25],
-    },
-    {
-      label: "Cat",
-      data: [12, 39, 10, 40],
-    },
-    {
-      label: "Others",
-      data: [22, 12, 30, 8],
-    },
-  ],
-});
-
-const chartOptions = {
-  scales: {
-    x: {
-      grid: {
-        display: false,
-      },
-    },
-  },
-};
 </script>
