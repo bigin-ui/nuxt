902fedbde2f002c8e5b40ff424e5b26b07ac7e44
Tam Mai <tam.mai@bigin.vn>
2022-11-30 21:07:27 +0700

    feat(layout): add layout

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <b-container class="min-h-screen">
+    <b-header>
+      <div class="flex items-center">
+        <img src="/favicon.png" class="w-8 h-8" />
+        <h1 class="text-3xl ml-2">Nuxt App</h1>
+      </div>
+    </b-header>
+    <b-main class="flex flex-col">
+      <slot />
+    </b-main>
+  </b-container>
+</template>
+
+<script setup lang="ts">
+import { useDark } from "@vueuse/core";
+
+useDark();
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,22 @@
 <template>
-  <b-container class="min-h-screen">
-    <b-main class="flex flex-col items-center justify-center">
-      <h1>BigIn</h1>
-      <p class="text-xl b-c-neutral-7 mt-4">Build trust. Make innovation.</p>
-      <b-button
-        tag="a"
-        href="https://bdsv3.netlify.app"
-        target="blank"
-        primary
-        :suffix-icon="ArrowRight"
-        class="mt-8"
-        >Documentation</b-button
-      >
-    </b-main>
-  </b-container>
+  <div class="flex-1 flex flex-col items-center justify-center">
+    <h1>BigIn</h1>
+    <p class="text-xl b-c-neutral-7 mt-4">Build trust. Make innovation.</p>
+    <b-button
+      tag="a"
+      href="https://bdsv3.netlify.app"
+      target="blank"
+      primary
+      :suffix-icon="External"
+      class="mt-8"
+      >Documentation</b-button
+    >
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ArrowRight } from "@bigin/icons-vue";
-import { useDark, useToggle } from "@vueuse/core";
+import { External } from "@bigin/icons-vue";
+import { useDark } from "@vueuse/core";
 
-const darkMode = useDark();
-const toggleDarkMode = () => useToggle(darkMode)();
+useDark();
 </script>
