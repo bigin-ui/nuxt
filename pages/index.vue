bb100a757fc6eee05de02b4ed90a945feec1729c
Tam Mai <tam.mai@bigin.vn>
2023-04-22 15:53:23 +0700

    feat(all): bump version

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1 class="c-primary-5">BigIn</h1>
+  <h1>BigIn</h1>
   <p class="text-xl b-c-neutral-7 mt-4">Build trust. Make innovation.</p>
   <div class="flex content-center mt-8">
     <b-button primary class="mr-4" @click.prevent="navigateTo('/demo')"
